cfb5de9375f3fbd5034579fc0c22a72344df07e6
Sanjeev Kumar <sanjeevksharma495@gmail.com>
2023-06-29 14:46:19 +0530

    Create Patch File

--- client3.py
+++ client3.py
@@ -19,6 +19,7 @@
 #  DEALINGS IN THE SOFTWARE.
 
 import json
+import time
 import random
 import urllib.request
 
@@ -29,20 +30,20 @@ QUERY = "http://localhost:8080/query?id={}"
 N = 500
 
 
-def getDataPoint(quote):
+def getDataPoint(quote, bid_price=None):
     """ Produce all the needed values to generate a datapoint """
     """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price)/2
     return stock, bid_price, ask_price, price
 
-
+ 
 def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    if(price_b == 0):
+        return
+    return price_a/price_b
 
 
 # Main
@@ -53,7 +54,8 @@ if __name__ == "__main__":
 
         """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
+            price: float
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(price['ABC'], price['DEF']))
